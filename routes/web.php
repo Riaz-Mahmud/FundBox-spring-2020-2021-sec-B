5016c3ee756a16a82b68f7605ef77c4e8c29b895
Debashish-Dey-DM <deydebashish71@gmail.com>
2021-06-18 02:05:12 +0600

    Admin half

--- routes/web.php
+++ routes/web.php
@@ -11,16 +11,30 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+*/ 
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/createadmin', function () {
-    // echo "it works";
-    return view('welcome');
+
+Route::get('/createAdmin', function () {
+    return view('Admin.createAdmin');
 });
 
-Route::get('/test', function () {
-    return view('welcome');
+Route::get('/adminEvent', function () {
+    return view('Admin.adminEvent');
+});
+Route::get('/orgEvent', function () {
+    return view('Admin.createOrgEvent');
+});
+
+Route::get('/verifyEvent',function(){
+    return view('Admin.verifyEvent');
+});
+
+Route::get('/blockOrg',function(){
+    return view('Admin.blockOrg');
+});
+Route::get('/transitionList',function(){
+    return view('Admin.transitionList');
 });
