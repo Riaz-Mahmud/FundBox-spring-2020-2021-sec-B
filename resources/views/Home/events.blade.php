71d156e4b4c48076fe3c6ba62487312789c3f7f5
RHShoumik <44612987+RHShoumik@users.noreply.github.com>
2021-06-21 05:19:13 +0600

    Phase-2 Home,about,contact,event Pages

--- resources/views/Home/events.blade.php
+++ resources/views/Home/events.blade.php
@@ -1,18 +1,11 @@
 @extends('Layout.masterlayout')
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-6">
-            <h1 style="margin-top : 90px;">Fundraising for the people and causes you care about</h1>
-            <h5>Get Start Today</h5>
-            <button type="button" class="btn btn-lg btn-primary" disabled>Start Donation</button>
-        </div>
-        <div class="col-6">
-            <img src="/images/pages/undraw_team_work_k80m.png" alt="team width="500" height="500"">
-        </div>
+<div class="event" style="background-color:#F2F4F4; height:200px; ">
+    <div class="container">
+        <h1 class="text-center" style="padding-top:70px;">Events</h1>
     </div>
 </div>
-<div class="event" style="background-color:#F2F4F4;">
+<div class="event">
     <div class="container">
         <div class="row" style="padding-bottom:30px;">
             <div class="col-12" >
@@ -53,23 +46,42 @@
         </div>
     </div>
 </div>
-
-<div class="status">
-    <div class="container" style="padding-bottom:30px;">
-        <h2 class="text-center" style="margin-top:30px;">We will save the world</h2>
-        <h6 class="text-center" style="margin-bottom:30px;">We have the strongest community</h6>
-        <div class="row">
-            <div class="col-4" style="text-align: center;">
-                <h2><b>86</b></h2>
-                <h3>Volunteers</h3>
-            </div>
-            <div class="col-4" style="text-align: center;">
-                <h2><b>47</b></h2>
-                <h3>Events</h3>
-            </div>
-            <div class="col-4" style="text-align: center;">
-                <h2><b>1.2M ৳</b></h2>
-                <h3>Total Donation</h3>
+<div class="event">
+    <div class="container">
+        <div class="row" style="padding-bottom:30px;">
+            <div class="col-12" >
+                <div class="row">
+                    <div class="col-4">
+                        <div class="card" style="width: 20rem;">
+                            <img class="card-img-top" style="height:13.4rem;" src="../../../images/pages/unnamed.jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Blood Donation</h5>
+                                <p class="card-text">A blood donation occurs when a person voluntarily has blood drawn and used for transfusions and/or made into biopharmaceutical medications by a process called fractionation</p>
+                                <a href="#" class="btn btn-primary">Join Now</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="card" style="width: 20rem;">
+                            <img class="card-img-top" style="height:13.4rem;" src="../../../images/pages/Article-Key-Image-453431968-800x420.jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Tree Plantation</h5>
+                                <p class="card-text">A blood donation occurs when a person voluntarily has blood drawn and used for transfusions and/or made into biopharmaceutical medications by a process called fractionation</p>
+                                <a href="#" class="btn btn-primary">Join Now</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="card" style="width: 20rem;">
+                            <img class="card-img-top" style="height:13.4rem;" src="../../../images/pages/dc-Cover-s2snl09nju40r1s1d4o2ced504-20170728024517.Medi.jpeg" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Education for everyone</h5>
+                                <p class="card-text">A blood donation occurs when a person voluntarily has blood drawn and used for transfusions and/or made into biopharmaceutical medications by a process called fractionation</p>
+                                <a href="#" class="btn btn-primary">Join Now</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -125,10 +137,54 @@
         </div>
     </div>
 </div>
-<div class="event"">
-    <div class="container" style="height:200px; text-align: center; margin-top:100px;">
-        <h2>Ready to Join us?</h2>
-        <button type="button" class="btn btn-success">Join with Us</button>
+<div class="event" style="background-color:#F2F4F4;">
+    <div class="container">
+        <div class="row" style="padding-bottom:30px;">
+            <div class="col-12" >
+                <div class="row">
+                    <div class="col-4">
+                        <div class="card" style="width: 20rem;">
+                            <img class="card-img-top" style="height:13.4rem;" src="../../../images/pages/317233_gettyimages .jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Save Nuhash</h5>
+                                <p class="card-text">A blood donation occurs when a person voluntarily has blood drawn and used for transfusions and/or made into biopharmaceutical medications by a process called fractionation</p>
+                                <div class="progress">
+                                    <div class="progress-bar" role="progressbar" style="width: 60%;" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">60%</div>
+                                </div>
+                                <h6 style="margin-top:10px;"> <b>৳ 6000 raised</b> of ৳ 10000</h6>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="card" style="width: 20rem;">
+                            <img class="card-img-top" style="height:13.4rem;" src="../../../images/pages/144367-cfmusnbhff-1594704365.jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Save the people</h5>
+                                <p class="card-text">A blood donation occurs when a person voluntarily has blood drawn and used for transfusions and/or made into biopharmaceutical medications by a process called fractionation</p>
+                                <div class="progress">
+                                    <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
+                                </div>
+                                <h6 style="margin-top:10px;"> <b>৳ 50000 raised</b> of ৳ 200000</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="card" style="width: 20rem;">
+                            <img class="card-img-top" style="height:13.4rem;" src="../../../images/pages/dc-Cover-s2snl09nju40r1s1d4o2ced504-20170728024517.Medi.jpeg" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Education for everyone</h5>
+                                <p class="card-text">A blood donation occurs when a person voluntarily has blood drawn and used for transfusions and/or made into biopharmaceutical medications by a process called fractionation</p>
+                                <div class="progress">
+                                    <div class="progress-bar" role="progressbar" style="width: 80%;" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">80%</div>
+                                </div>
+                                <h6 style="margin-top:10px;"> <b>৳ 8000 raised</b> of ৳ 10000</h6>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
